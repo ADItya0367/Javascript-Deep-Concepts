d22da53601bf22a1d08f34d371cbd42c991eff0e
ADItya0367 <21egjcs001@gitjaipur.com>
2024-03-26 12:29:41 +0530

    How to call  an API through XMLHttpRequest by GET Method

---
+++ index_Lecture_02.js
@@ -0,0 +1,3 @@
+//here we are using external javascript file to write the code see their how we are attaching the script file 
+
+// 1. Create a new index_Lecture_02.js file and write a program that does the following:
